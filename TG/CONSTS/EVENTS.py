1f3335dd1e9b3b6c94fbcc2c3efcb9d3012cf85e
Astrocatling <konstantdarl@gmail.com>
2022-08-07 02:44:10 +0300

    cleaning 2

--- TG/CONSTS/EVENTS.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-EVENTS = {'FULL_NAME': 0, 'ADDRESSES': 1, 'END': 2}
-
-# Присваиваем переменную качестве возвращаемого объекта при импорте
-sys.modules[__name__] = EVENTS
--- TG/CONSTS/PUP_STATES.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-PUP_STATES = {'FULL_NAME': 0, 'ADDRESSES': 1, 'END': 2}
-
-# Присваиваем переменную качестве возвращаемого объекта при импорте
-sys.modules[__name__] = PUP_STATES
